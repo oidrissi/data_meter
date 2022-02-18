600a31dc9f7c807f8939b4affe95ab5af3442a9c
unknown <oyazami@gmail.com>
2022-02-18 04:55:07 +0100

    smaller, less noise

--- data_monitor.py
+++ data_monitor.py
@@ -8,7 +8,7 @@ from requests import get
 start_val = 0
 
 def set_data_to_consume():
-    print("How Much Data Do you Have left?")
+    print("How Much Data Do you Have Left? (In Go)")
     requested_data = input()
     return (requested_data)
 
@@ -19,29 +19,26 @@ def data(start_val):
     while True:
         current_val = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
         result = current_val - start_val + result
-        return (str(convert_to_mo(result)))
+        return (convert_to_mo(result))
 
 def get_ext_ip():
     ip = get('https://api.ipify.org').content.decode('utf8')
     return ('My public IP address is: {}'.format(ip))
 
+# days_until_renewal = input()
+
+# def prediction_end():
+    
+
 ip = get_ext_ip()
-max_data = set_data_to_consume()
-data_left = +
+max_data = float(set_data_to_consume()) * 1000
+
 def main():
     kiss = data(start_val)
-    display_label['text'] = kiss + ' || Mo Consumed ' + '\n' + (str(ip))
+    left = max_data - kiss
+    display_label['text'] = str(kiss) + ' || Mo Consumed ' + '\n' + (str(ip)) + '\n' + (str(left)) + ' || Mo Left '
     display_label.pack()
     app.after(5, main)
-        
-def is_connected():
-    try:
-        # connect to the host -- tells us if the host is actually reachable
-        socket.create_connection(("www.google.com", 80))
-        return True
-    except OSError:
-        pass
-    return False
 
 def convert_to_mo(value):
     #1 octet = 1 byte = 8 bits // 1024 bytes = 8192 bits = 1 kb;
@@ -49,10 +46,10 @@ def convert_to_mo(value):
 
 app = Tk()
 app.title("Data Consumption Monitor")
-app.geometry('420x69')
+app.geometry('250x40')
 app.configure(background = 'black')
 frame = Frame(app)
-display_label = Label(frame, font = 'Terminal 15', bg = 'black', fg = '#20C20E')
+display_label = Label(frame, font = 'Terminal 6', bg = 'black', fg = '#20C20E')
 frame.pack(anchor = CENTER)
 main()
 app.attributes('-topmost',True)
